5015db4255fa9ea7596399cb61cc4fa978247dce
rcaromi <raied.caromi@nist.gov>
2019-08-15 13:28:00 -0400

    added duration

--- src/dsp/interferenceAdder.m
+++ src/dsp/interferenceAdder.m
@@ -1,3 +1,35 @@
+...%% Legal Disclaimer
+...% NIST-developed software is provided by NIST as a public service. 
+...% You may use, copy and distribute copies of the software in any medium,
+...% provided that you keep intact this entire notice. You may improve,
+...% modify and create derivative works of the software or any portion of
+...% the software, and you may copy and distribute such modifications or
+...% works. Modified works should carry a notice stating that you changed
+...% the software and should note the date and nature of any such change.
+...% Please explicitly acknowledge the National Institute of Standards and
+...% Technology as the source of the software.
+...% 
+...% NIST-developed software is expressly provided "AS IS." NIST MAKES NO
+...% WARRANTY OF ANY KIND, EXPRESS, IMPLIED, IN FACT OR ARISING BY
+...% OPERATION OF LAW, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTY
+...% OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, NON-INFRINGEMENT
+...% AND DATA ACCURACY. NIST NEITHER REPRESENTS NOR WARRANTS THAT THE
+...% OPERATION OF THE SOFTWARE WILL BE UNINTERRUPTED OR ERROR-FREE, OR
+...% THAT ANY DEFECTS WILL BE CORRECTED. NIST DOES NOT WARRANT OR MAKE ANY 
+...% REPRESENTATIONS REGARDING THE USE OF THE SOFTWARE OR THE RESULTS 
+...% THEREOF, INCLUDING BUT NOT LIMITED TO THE CORRECTNESS, ACCURACY,
+...% RELIABILITY, OR USEFULNESS OF THE SOFTWARE.
+...% 
+...% You are solely responsible for determining the appropriateness of
+...% using and distributing the software and you assume all risks
+...% associated with its use, including but not limited to the risks and
+...% costs of program errors, compliance with applicable laws, damage to 
+...% or loss of data, programs or equipment, and the unavailability or
+...% interruption of operation. This software is not intended to be used in
+...% any situation where a failure could cause risk of injury or damage to
+...% property. The software developed by NIST employees is not subject to
+...% copyright protection within the United States.
+
 classdef interferenceAdder
     %RADARINTERFERENCE Summary of this class goes here
     %   Detailed explanation goes here
@@ -116,7 +148,7 @@ classdef interferenceAdder
                 %wPowdBmMat=repmat(rangedBm,[length(app.allSigs),1]);
                 allWaveformTable=addvars(allWaveformTable,randomNoisePowdBmOrSNRdB,repmat(this.inParameters.wgn.noisePowerdBmP1MHzFixed,[length(waveformCell),1]),'NewVariableNames',{'SNR','NoisePowerdBmPerMHz'});
             end
-            
+
             %                 for I=1:length(waveformCell)
             %                     wpow{I,1}=rangedBm(randi([1,length(rangedBm)]))+pow2db((allWaveformTable.SamplingFrequency(I)/1e6));%+db2pow(3);%%%check check
             %                 end
@@ -145,10 +177,10 @@ classdef interferenceAdder
                 
                 
                 waveformCell=[waveformCell;noiseWaveformCell];
-                
+                            
                 numericOrLogicalVars = varfun(@(x) isnumeric(x)|islogical(x),allWaveformTable,'output','uniform');
                 newCellforWGN=cell(height(allWaveformTable),width(allWaveformTable));
-                newCellforWGN(repmat(~numericOrLogicalVars,[height(allWaveformTable),1]))={'NAN'};
+                newCellforWGN(repmat(~numericOrLogicalVars,[height(allWaveformTable),1]))={'NaN'};
                 varsToChange={'SamplingFrequency','radarStatus','NoisePowerdBmPerMHz'};
                 varsToChangeLogical=and(numericOrLogicalVars,ismember(allWaveformTable.Properties.VariableNames,varsToChange));
                 newCellforWGN(repmat(~varsToChangeLogical&numericOrLogicalVars,[height(allWaveformTable),1]))={NaN};
@@ -161,7 +193,9 @@ classdef interferenceAdder
                 
             end
             
-            
+            durationSec=cellfun(@length,waveformCell)./allWaveformTable.SamplingFrequency;
+            allWaveformTable=addvars(allWaveformTable,durationSec);
+            % randomize twice 
             newIndex=randperm(length(waveformCell)).';
             waveformCell=waveformCell(newIndex,:);
             allWaveformTable=allWaveformTable(newIndex,:);
